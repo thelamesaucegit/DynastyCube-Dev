82a2d668cf9fa3d040814a5d2a7224b63dec6d00
thelamesaucegit <100131464+thelamesaucegit@users.noreply.github.com>
2025-07-26 10:20:32 -0500

    updated link targets

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -31,27 +31,27 @@ const Navigation: React.FC = () => {
             </Link>
           </li>
           <li>
-            <Link href="" className="nav-link" role="menuitem">
+            <Link href="/account" className="nav-link" role="menuitem">
               My Account
             </Link>
           </li>
           <li>
-            <Link href="" className="nav-link" role="menuitem">
+            <Link href="/teams" className="nav-link" role="menuitem">
               Teams
             </Link>
           </li>
           <li>
-            <Link href="" className="nav-link" role="menuitem">
+            <Link href="/pools" className="nav-link" role="menuitem">
               Pools
             </Link>
           </li>
           <li>
-            <Link href="" className="nav-link" role="menuitem">
+            <Link href="/schedule" className="nav-link" role="menuitem">
               Schedule
             </Link>
           </li>
           <li>
-            <Link href="" className="nav-link" role="menuitem">
+            <Link href="/vote" className="nav-link" role="menuitem">
               Vote
             </Link>
           </li>
