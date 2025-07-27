dd3009717d11df666fc20d965814b86d09aeda45
thelamesaucegit <100131464+thelamesaucegit@users.noreply.github.com>
2025-07-27 10:21:21 -0500

    fix page syntax and layout

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -1,5 +1,4 @@
-
-// src/app/page.tsx
+// src/app/teams/page.tsx
 import React from 'react';
 import Image from 'next/image';
 import Layout from '@/components/Layout';
@@ -9,38 +8,52 @@ export default function Page() {
     <Layout>
       <div className="hero-section">
         <h3>Teams</h3>
-    
-        
         <p className="hero-subtitle">Teams</p>
       </div>
-<p class="align-center">Meet the Teams<br><br>
-  <br><br>
-    🌟 <strong>Alara Shards</strong> 🌟
-    <br>
-      <i>"Why not both?"</i><br><br>
-        
-    ⛩ <strong>Kamigawa Ninja</strong> ⛩
-        <br><i>"Omae wa mou shindeiru."</i><br><br>
-          
-     🧟 <strong>Innistrad Creeps</strong> 🧟
-          <br><i>"Graveyard, Gatekeep, Girlboss"</i><br><br>
-            
-     🌞 <strong>Theros Demigods<strong> 🌞
-              <br><i>”The Fates will decide”</i><br><br>
-                
-      🔗 <strong>Ravnica Guildpact</strong> 🔗
-                <br><i>“A Championship is won and lost before ever entering the battlefield”</i><br><br>
-                  
-      👽 <strong>Lorwyn Changelings</strong> 👽
-                  <br><i>”Expect the unexpected”</i><br><br>
-                    
-       💠  <strong>Zendikar Hedrons</strong> 💠 
-                    <br><i>"Good Vibes, No Escape"</i><br><br>
-                      
-       🐲 <strong>Tarkir Dragons</strong> 🐲
-                      <br><i>"No cost too great"</i></p>
-      
-      
+
+      <p className="align-center">
+        Meet the Teams
+        <br /><br />
+        🌟 <strong>Alara Shards</strong> 🌟
+        <br />
+        <i>"Why not both?"</i>
+        <br /><br />
+
+        ⛩ <strong>Kamigawa Ninja</strong> ⛩
+        <br />
+        <i>"Omae wa mou shindeiru."</i>
+        <br /><br />
+
+        🧟 <strong>Innistrad Creeps</strong> 🧟
+        <br />
+        <i>"Graveyard, Gatekeep, Girlboss"</i>
+        <br /><br />
+
+        🌞 <strong>Theros Demigods</strong> 🌞
+        <br />
+        <i>"The Fates will decide"</i>
+        <br /><br />
+
+        🔗 <strong>Ravnica Guildpact</strong> 🔗
+        <br />
+        <i>"A Championship is won and lost before ever entering the battlefield"</i>
+        <br /><br />
+
+        👽 <strong>Lorwyn Changelings</strong> 👽
+        <br />
+        <i>"Expect the unexpected"</i>
+        <br /><br />
+
+        💠 <strong>Zendikar Hedrons</strong> 💠
+        <br />
+        <i>"Good Vibes, No Escape"</i>
+        <br /><br />
+
+        🐲 <strong>Tarkir Dragons</strong> 🐲
+        <br />
+        <i>"No cost too great"</i>
+      </p>
+
       <div className="content-divider"></div>
     </Layout>
   );
