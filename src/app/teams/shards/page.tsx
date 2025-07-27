5a99c7ec5332a60e638af5bf569348ae910e6d54
thelamesaucegit <100131464+thelamesaucegit@users.noreply.github.com>
2025-07-27 17:41:15 -0500

    Blank page

--- src/app/teams/shards/page.tsx
+++ src/app/teams/shards/page.tsx
@@ -1,1 +1,20 @@
+// src/app/page.tsx
+import React from 'react';
+//import Image from 'next/image';
+import Layout from '@/components/Layout';
 
+export default function HomePage() {
+  return (
+    <Layout>
+      <div className="hero-section">
+       
+        </div>
+        
+        {/* <div className="logo-divider"></div> */}
+        
+        
+      
+      <div className="content-divider"></div>
+    </Layout>
+  );
+}
