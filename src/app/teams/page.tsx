f78c5851323ac373faa8dc5d112afed74ff1894b
thelamesaucegit <100131464+thelamesaucegit@users.noreply.github.com>
2025-07-27 09:38:10 -0500

    Add Team details

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -11,9 +11,34 @@ export default function Page() {
         <h3>Teams</h3>
     
         
-        <p className="hero-subtitle">A collaborative, living draft format</p>
+        <p className="hero-subtitle">Teams</p>
       </div>
-
+<p class="align-center">Meet the Teams<br><br>
+  <br><br>
+    🌟 <strong>Alara Shards</strong> 🌟
+    <br>
+      <i>"Why not both?"</i><br><br>
+        
+    ⛩ <strong>Kamigawa Ninja</strong> ⛩
+        <br><i>"Omae wa mou shindeiru."</i><br><br>
+          
+     🧟 <strong>Innistrad Creeps</strong> 🧟
+          <br><i>"Graveyard, Gatekeep, Girlboss"</i><br><br>
+            
+     🌞 <strong>Theros Demigods<strong> 🌞
+              <br><i>”The Fates will decide”</i><br><br>
+                
+      🔗 <strong>Ravnica Guildpact</strong> 🔗
+                <br><i>“A Championship is won and lost before ever entering the battlefield”</i><br><br>
+                  
+      👽 <strong>Lorwyn Changelings</strong> 👽
+                  <br><i>”Expect the unexpected”</i><br><br>
+                    
+       💠  <strong>Zendikar Hedrons</strong> 💠 
+                    <br><i>"Good Vibes, No Escape"</i><br><br>
+                      
+       🐲 <strong>Tarkir Dragons</strong> 🐲
+                      <br><i>"No cost too great"</i></p>
       
       
       <div className="content-divider"></div>
