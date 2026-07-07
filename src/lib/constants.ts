9c483486842e5d1ef091fb86c34c76f89ea558c5
thelamesaucegit <100131464+thelamesaucegit@users.noreply.github.com>
2026-07-07 14:56:54 -0500

    Delete src/lib/constants.ts

--- src/lib/constants.ts
+++
@@ -1,8 +0,0 @@
-// /src/lib/constants.ts
-
-export const SEASON_4_ID = '4b1d9936-bf5e-4ee5-bd56-741a7c12307e';
-
-export const EXPANSION_TEAM_IDS = [
-    '2bfc34c2-045b-4ac7-872b-05aeebd4c53b', // Changelings/Mimics
-    '624e3ecc-672d-4ee4-8a4a-7be8c61d39e9'  // Tarkir Dragons
-];
